8d511017344db54994bef4cb378f192a97108c6a
Dana Wardhiana <artaputra95@gmail.com>
2024-03-11 01:41:48 +0700

    fix: add decoded

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -7,15 +7,14 @@ const verifyToken = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ error: true, message: 'Token not provided' });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       console.error('JWT verification error:', err);
       return res.status(403).json({ error: true, message: 'Invalid token' });
     }
-    req.user = user;
+    req.user = decoded.user; // Assuming the user ID is stored under the 'user' property in the token payload
     next();
   });
 };
 
-
-module.exports = verifyToken
+module.exports = verifyToken;
